19a0926818806e6bf015bf932683d9a413954dbc
Anna Andreikiv <andreikiv.ann@gmail.com>
2022-02-03 22:11:28 +0200

    add button done

---
+++ src/components/ButtonAddCard/ButtonAddCard.jsx
@@ -0,0 +1,11 @@
+import "../../utils/variables.css";
+import s from "./ButtonAddCard.module.css";
+const ButtonAddCard = () => {
+  return (
+    <button className={s.ButtonAddCard} type="button">
+      +
+    </button>
+  );
+};
+
+export default ButtonAddCard;
--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,9 +1,11 @@
+import ButtonAddCard from "../ButtonAddCard/ButtonAddCard";
 import Card from "./Card/Card";
 import s from "./Container.module.css";
 const Container = () => {
   return (
     <div className={s.container}>
       <Card />
+      <ButtonAddCard />
     </div>
   );
 };
