2c4d0f209e5e5b6aff167a9a9d5c0d1a809f8c05
corund1976 <corund1976@gmail.com>
2022-02-05 13:49:38 +0200

    1.подправил разметку страниц 2.подправил разметку карточек 3.связал хедер с карточками и страницей 4.подкорректировал стили

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,8 @@
 import Container from "../../components/Container";
 import Header from "../../components/Header/Header";
 
-import Card from "../../components/Card";
+import CardsToday from "../../components/CardsToday";
+import CardsTomorrow from "../../components/CardsTomorrow";
 import ButtonAddCard from "../../components/ButtonAddCard";
 
 import s from './HomePage.module.css'
@@ -9,13 +10,14 @@ import s from './HomePage.module.css'
 function HomePage() {
   return (
     <section className={s.dashboard}>
+      <Header />
       <Container>
-          <Header />
-          <Card />
+          <CardsToday />
+          <CardsTomorrow />
           <ButtonAddCard />
       </Container>
     </section>
   );
 }
 
-export default HomePage
+export default HomePage;
