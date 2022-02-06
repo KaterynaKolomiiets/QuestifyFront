036d98393d63465faff07d8f809c53eaf22ffe91
sandra9ermak <sandra1ermak@gmail.com>
2022-02-06 16:25:43 +0200

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,29 +4,33 @@ import BtnLogout from "../../images/btn-logout.png";
 
 import Avatar from "@mui/material/Avatar";
 import { blueGrey } from "@mui/material/colors";
-import { useDispatch, useSelector } from 'react-redux';
-import {getUser} from '../../redux/user/selectors'
-import {userLogout} from '../../redux/user/operation'
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "../../redux/user/selectors";
+import { userLogout } from "../../redux/user/operation";
 
 const Header = () => {
   const dispatch = useDispatch();
   const user = useSelector(getUser);
 
   const onLogout = () => {
-    dispatch(userLogout({ email: user.email, password: user.password}))
-  }
+    dispatch(userLogout({ email: user.email, password: user.password }));
+  };
+
+  const userEmail = user.email;
+  const avatarLetter = userEmail.substr(0, 1).toUpperCase();
+
   return (
     <header className={s.headerSection}>
       <Container>
         <div className={s.header}>
           <p className={s.logo}>Questify</p>
           <div className={s.userData}>
-            <Avatar sx={{ bgcolor: blueGrey[800] }}>U</Avatar>
-            <p className={s.userName}>UserName</p>
+            <Avatar sx={{ bgcolor: blueGrey[800] }}>{avatarLetter}</Avatar>
+            <p className={s.userName}>{user.email}</p>
           </div>
           <div className={s.userDataMobile}>
             <Avatar sx={{ bgcolor: blueGrey[800] }} className={s.avatarMobile}>
-              U
+              {avatarLetter}
             </Avatar>
             <button type="button" className={s.BtnLogout} onClick={onLogout}>
               <img src={BtnLogout} alt="button-logout" />
