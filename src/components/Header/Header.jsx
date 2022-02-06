40089dc8e5916ce62e03921410b7bead960589f6
sandra9ermak <sandra1ermak@gmail.com>
2022-02-06 20:29:33 +0200

    2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,37 @@
 import s from "./Header.module.css";
 import Container from "../Container";
+import ModalWindow from "../modal/ModalWindow/ModalWindow";
 import BtnLogout from "../../images/btn-logout.png";
-
+import challengeIcon from "../../images/challengeIcon.png";
 import Avatar from "@mui/material/Avatar";
 import { blueGrey } from "@mui/material/colors";
+
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { useEffect } from "react";
 import { getUser } from "../../redux/user/selectors";
 import { userLogout } from "../../redux/user/operation";
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
   const user = useSelector(getUser);
+  const toggleModal = () => setShowModal((prevState) => !prevState);
+
+  const modalEscape = (e) => {
+    if (e.code === "Escape") {
+      toggleModal();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", modalEscape);
+
+    return () => {
+      window.removeEventListener("keydown", modalEscape);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const onLogout = () => {
     dispatch(userLogout({ email: user.email, password: user.password }));
@@ -29,15 +50,38 @@ const Header = () => {
             <p className={s.userName}>{user.email}</p>
           </div>
           <div className={s.userDataMobile}>
-            <Avatar sx={{ bgcolor: blueGrey[800] }} className={s.avatarMobile}>
-              {avatarLetter}
-            </Avatar>
-            <button type="button" className={s.BtnLogout} onClick={onLogout}>
+            <div className={s.avatarMobile}>
+              <Avatar sx={{ bgcolor: blueGrey[800] }}>{avatarLetter}</Avatar>
+            </div>
+            <button type="button" className={s.challengeIcon}>
+              <img
+                src={challengeIcon}
+                alt="challengeIcon"
+                className={s.challengeIconPng}
+              />
+            </button>
+            <button type="button" className={s.BtnLogout} onClick={toggleModal}>
               <img src={BtnLogout} alt="button-logout" />
+              {/* <svg width="20" height="20" className={s.svgLogout}>
+                <use href="../../icons/sprite.svg#icon-logout"></use>
+              </svg> */}
             </button>
           </div>
         </div>
       </Container>
+      {showModal && (
+        <ModalWindow toggleModal={toggleModal}>
+          <p className={s.modalTitle}>Are you sure you want to exit?</p>
+          <div className={s.btnDiv}>
+            <button type="button" className={s.modalBtn} onClick={onLogout}>
+              Yes
+            </button>
+            <button type="button" className={s.modalBtn} onClick={toggleModal}>
+              No
+            </button>
+          </div>
+        </ModalWindow>
+      )}
     </header>
   );
 };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,16 +5,16 @@ import CardsToday from "../../components/CardsToday";
 import CardsTomorrow from "../../components/CardsTomorrow";
 import ButtonAddCard from "../../components/ButtonAddCard";
 
-import s from './HomePage.module.css'
+import s from "./HomePage.module.css";
 
 function HomePage() {
   return (
     <section className={s.dashboard}>
       <Header />
       <Container>
-          <CardsToday />
-          <CardsTomorrow />
-          <ButtonAddCard />
+        <CardsToday />
+        <CardsTomorrow />
+        <ButtonAddCard />
       </Container>
     </section>
   );
