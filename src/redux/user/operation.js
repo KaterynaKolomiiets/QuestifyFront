e7ba056a7fc27eaba945e76973a043e07e3ff5a0
Iurii Bilonog <iuriibilonog@gmail.com>
2022-02-10 19:02:12 +0200

    save2

--- src/redux/user/operation.js
+++ src/redux/user/operation.js
@@ -62,6 +62,7 @@ export const userLogout = createAsyncThunk(
       document.cookie = 'refreshToken=-1;expires=Thu, 01 Jan 1970 00:00:01 GMT';
       // token.unset();
       localStorage.removeItem('token');
+      console.log('ok');
       console.log('data', data);
       return data;
     } catch (error) {
