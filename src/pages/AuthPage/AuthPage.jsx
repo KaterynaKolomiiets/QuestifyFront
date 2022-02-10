9b85943ca26e98bcf02f70b3451d1e6421cfe942
corund1976 <corund1976@gmail.com>
2022-02-10 23:47:14 +0200

    сдвиг позиционки при разрешении более макета 1280

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -6,7 +6,6 @@ import Container from '../../components/Container';
 import AuthForm from '../../components/AuthForm';
 import { getError } from '../../redux/user/selectors';
 
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import s from './AuthPage.module.css';
 
 function AuthPage() {
